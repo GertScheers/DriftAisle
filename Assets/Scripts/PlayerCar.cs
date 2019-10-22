bdf24f8cf1dfd3bd3943be6ce02542ab967de1ef
Gitjeshub <38493098+Gitjeshub@users.noreply.github.com>
2019-10-22 10:05:10 +0200

    First sync to laptop

---
+++ Assets/Scripts/PlayerCar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCar : MonoBehaviour
+{
+    Quaternion targetRotation;
+    Rigidbody _rigidBody;
+    [SerializeField] float turnspeed = 5;
+    [SerializeField] float acceleration = 2;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        SetRotationPoint();
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnspeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+
+        float accelerationInput = acceleration * (Input.GetMouseButton(0) ? 1 : Input.GetMouseButton(1) ? -1 : 0) * Time.fixedDeltaTime;
+        _rigidBody.AddRelativeForce(Vector3.forward * accelerationInput);
+    }
+
+    private void SetRotationPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        
+        if(plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TopdownCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform observable;
+    [SerializeField]
+    float aheadSpeed;
+    [SerializeField]
+    float followDamping;
+    [SerializeField]
+    float cameraHeight;
+
+    Rigidbody _observableRigidBody;
+
+    private void Start()
+    {
+        _observableRigidBody = observable.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (observable == null)
+            return;
+
+        Vector3 targetPos = observable.position + Vector3.up * cameraHeight + _observableRigidBody.velocity * aheadSpeed;
+        transform.position = Vector3.Lerp(transform.position, targetPos, followDamping * Time.deltaTime);
+    }
+}
